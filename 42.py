60c3d973d2a63bc0e9e2910ab72d64092146eb25
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2024-08-09 20:05:54 -0300

    Add files via upload

---
+++ 42.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+root = Tk()
+root.title('Título da nossa janela')
+root.geometry('500x400+100+100')
+root.config(bg='gray40')
+root.resizable(True, True)
+root.attributes('-alpha', 0.7)
+
+frame1 = Frame(root).place(relx=0.01, rely=0.01, relwidth=0.98, relheight=0.48)
+botao1 = Button(frame1, text='Novo', bg='gray35', fg='white', font=('verdana', 12, 'bold')).place(relx=0.05, rely=0.05, relwidth=0.15, relheight=0.15)
+botao2 = Button(frame1, text='Alterar', bg='gray35', fg='white', font=('verdana', 12, 'bold')).place(relx=0.21, rely=0.05, relwidth=0.15, relheight=0.15)
+botao3 = Button(frame1, text='Apagar', bg='gray35', fg='white', font=('verdana', 12, 'bold')).place(relx=0.37, rely=0.05, relwidth=0.15, relheight=0.15)
+botao4 = Button(frame1, text='Buscar', bg='gray35', fg='white', font=('verdana', 12, 'bold')).place(relx=0.53, rely=0.05, relwidth=0.15, relheight=0.15)
+frame2 = Frame(root).place(relx=0.01, rely=0.5, relwidth=0.98, relheight=0.48)
+
+root.mainloop()
