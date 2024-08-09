15e36b084e33eb8a752ba6ade85c62abeb09102a
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2024-08-09 19:14:29 -0300

    Add files via upload

---
+++ 40.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+root = Tk()
+root.title('Título da nossa janela')
+root.geometry('300x200+100+100')
+root.config(bg='gray40')
+root.resizable(True, True)
+
+frame1 = Frame(root)
+frame2 = Frame(root)
+label1 = Label(frame1, text='label1')
+label2 = Label(frame1, text='label2')
+
+frame1.place(relx=0.01, rely=0.01, relwidth=0.98, relheight=0.48)
+frame2.place(relx=0.01, rely=0.5, relwidth=0.98, relheight=0.48)
+
+root.mainloop()
